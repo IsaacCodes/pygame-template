de34b9ee398f79bfb12a1a3a7e57e26b73379f08
IsaacCodes <179248168+IsaacCodes@users.noreply.github.com>
2024-10-14 15:45:49 +0000

    Finished template formatting

--- main.py
+++ main.py
@@ -4,23 +4,10 @@ pg.init()
 width, height = 500, 500
 
 screen = pg.display.set_mode((width, height))
-pg.display.set_caption("Test")
+pg.display.set_caption("Pygame Template")
 
 objects = pg.sprite.Group()
 
-class Object(pg.sprite.Sprite):
-  def __init__(self, size, color, pos):
-    super().__init__()
-    objects.add(self)
-
-    self.image = pg.Surface(size)
-    self.image.fill(color)
-
-    self.rect = self.image.get_rect()
-    self.rect.center = pos
-
-
-red_box = Object((50, 50), "red", (width/2, height/2))
 
 #Vars
 clock = pg.time.Clock()
@@ -35,15 +22,7 @@ while running:
     if event.type == pg.QUIT: 
       running = False
 
-  keys = pg.key.get_pressed()
-  up = keys[pg.K_w] or keys[pg.K_UP]
-  left = keys[pg.K_a] or keys[pg.K_LEFT]
-  down = keys[pg.K_s] or keys[pg.K_DOWN]
-  right = keys[pg.K_d] or keys[pg.K_RIGHT]
-
-  red_box.rect.x += (right - left) * 5
-  red_box.rect.y += (down - up) * 5
-
+  
   #BG
   screen.fill((255, 255, 255))
   #Objects
